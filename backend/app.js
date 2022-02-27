84353cbb200966374ccb0a8a6ee20ce3c3c91ccb
Geof73 <gpiratmail@gmail.com>
2022-02-27 22:58:19 +0100

    ajout protection route controller

--- backend/app.js
+++ backend/app.js
@@ -8,7 +8,7 @@ const userRoutes = require('./routes/user');
 mongoose.connect('mongodb+srv://admin:nimda@cluster0.pxgga.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
   })
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch(() => console.log('Connexion à MongoDB échouée !'));
@@ -19,7 +19,7 @@ app.use(express.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization, default');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
   next();
 });
--- backend/controllers/Sauce.js
+++ backend/controllers/Sauce.js
@@ -35,25 +35,38 @@ exports.getOneSauce = (req, res, next) => {
 };
 
 exports.modifySauce = (req, res, next) => {
-  const sauceObject = req.file ?
-    {
-      ...JSON.parse(req.body.sauce),
-      imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
-    } : { ...req.body };
-  Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
-    .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
-    .catch(error => res.status(400).json({ error }));
+  Sauce.findOne({ _id: req.params.id })
+    .then((sauce) => {
+      if (sauce.userId === req.token.userId) {
+        const sauceObject = req.file ?
+          {
+            ...JSON.parse(req.body.sauce),
+            imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+          } : { ...req.body };
+        Sauce.updateOne({ _id: req.params.id }, { ...sauceObject, _id: req.params.id })
+          .then(() => res.status(200).json({ message: 'Sauce modifiée !' }))
+          .catch(error => res.status(400).json({ error }));
+      }
+      else {
+        throw new Error('Unauthorized request.', { statusCode: 403 })
+      }
+    })
 };
 
 exports.deleteSauce = (req, res, next) => {
   Sauce.findOne({ _id: req.params.id })
     .then((sauce) => {
-      const filename = sauce.imageUrl.split('/images/')[1];
-      fs.unlink(`images/${filename}`, () => {
-        Sauce.deleteOne({ _id: req.params.id })
-          .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
-          .catch(error => res.status(400).json({ error }));
-      });
+      if (sauce.userId === req.token.userId) {
+        const filename = sauce.imageUrl.split('/images/')[1];
+        fs.unlink(`images/${filename}`, () => {
+          Sauce.deleteOne({ _id: req.params.id })
+            .then(() => res.status(200).json({ message: 'Sauce supprimée !' }))
+            .catch(error => res.status(400).json({ error }));
+        });
+      }
+      else {
+        throw new Error('Unauthorized request.', { statusCode: 403 })
+      }
     })
     .catch(error => res.status(500).json({ error }));
 };
@@ -102,7 +115,7 @@ exports.sauceLike = (req, res, next) => {
             { $pull: { usersLiked: req.body.userId }, $inc: { likes: -1 } }
           )
             .then((sauce) => {
-              res.status(200).json({ message: "Like deleted" });
+              res.status(200).json({ message: "Like supprimé" });
             })
             .catch((error) => res.status(400).json({ error }));
         } else if (sauce.usersDisliked.includes(req.body.userId)) {
@@ -114,7 +127,7 @@ exports.sauceLike = (req, res, next) => {
             }
           )
             .then((sauce) => {
-              res.status(200).json({ message: "Dislike deleted" });
+              res.status(200).json({ message: "Dislike supprimé" });
             })
             .catch((error) => res.status(400).json({ error }));
         }
